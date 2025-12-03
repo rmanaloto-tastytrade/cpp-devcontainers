b9bcc208475693ff255ee707f91c93dfb45c9fb7
Ray Manaloto <ray.manaloto@tastytrade.com>
2025-12-02 22:11:34 -0600

    Fix boost-ut overlay license path

--- vcpkg-overlays/boost-ext/ports/boost-ut/portfile.cmake
+++ vcpkg-overlays/boost-ext/ports/boost-ut/portfile.cmake
@@ -26,5 +26,5 @@ vcpkg_cmake_config_fixup(
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 vcpkg_install_copyright(
-    FILE_LIST "${SOURCE_PATH}/LICENSE"
+    FILE_LIST "${SOURCE_PATH}/LICENSE.md"
 )
